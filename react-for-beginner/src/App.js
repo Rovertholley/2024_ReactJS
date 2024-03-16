57cd5c5a23f00c6aa209e446d06143b8b63bd32e
Rovertholley <pjmpiop@gmail.com>
2024-03-16 11:14:30 +0900

    6강 끝!

--- react-for-beginner/src/App.js
+++ react-for-beginner/src/App.js
@@ -1,12 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Button from "./Button";
 
 function App() {
   const [counter,setValue] = useState(0);
+  const [keyword,setKeyword] = useState("");
   const onClick = () => setValue((prev)=>prev+1);
-  
+  const onChange = (event) => setKeyword(event.target.value);
+  useEffect(() =>{
+    console.log("CALL THE API");
+
+  },[]);
+  useEffect(() =>{
+    console.log("Search For ",keyword);
+
+  },[keyword]);
   return (
     <div>
+      <input 
+        onChange={onChange}
+        value={keyword}
+        type="text"
+        placeholder="Search"></input>
       <h1>{counter}</h1>
       <button onClick={onClick}>sex</button>
     </div> 
